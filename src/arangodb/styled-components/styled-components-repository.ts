17f5f15d222d4c92c7b63beaa9fd900f73f7b17f
Cader Hancock <Cader.Hancock@gmail.com>
2021-05-16 23:20:39 -0700

    Another one.

--- src/arangodb/styled-components/styled-components-repository.ts
+++ src/arangodb/styled-components/styled-components-repository.ts
@@ -1,10 +1,11 @@
 import { aql } from 'arangojs'
 import { Connect } from '../connection'
-import { styled } from 'styled-components'
+// import { styled } from 'styled-components'
+// import { FieldTypes } from '../../fields/FieldTypes'
 
 // TODO So we're going to need to figure out the exact paramerters for component style identification
 // maybe just field type, and collation id but that depends on how specific the field type is.
-export async function GetStyledDivById(id) {
+export async function GetStyledFieldByIdentifier() {
     const db = Connect()
     db.collection("styled-components")
     const query = aql`FOR component in styled-components FILTER component.id == ${id} RETURN component`
