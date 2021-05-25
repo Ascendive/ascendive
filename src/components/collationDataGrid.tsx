a0f993b7f024415adc78e401a699b67584aaaab9
Cader Hancock <Cader.Hancock@gmail.com>
2021-05-25 16:48:45 -0700

    Stubed out the CollationDataGrid

---
+++ src/components/collationDataGrid.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Collatable } from '../collation/Collatable';
+
+export function CollationDataGrid(data: Collatable[]): JSX.Element {
+    return (
+        <>
+            {/*TODO: Implement this thing. I hope this is easier than I think.  */}
+        </>
+    );
+
+}
