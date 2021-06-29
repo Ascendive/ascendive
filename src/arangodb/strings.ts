dd831ecedf418176482c91ec88a2c0ffa03edcca
Cader Hancock <Cader.Hancock@gmail.com>
2021-06-29 16:54:15 -0700

    ok then

--- src/arangodb/strings.ts
+++ src/arangodb/strings.ts
@@ -2,4 +2,4 @@ export const DatabaseUrl = "13.66.154.199:8529"
 
 export const DatabaseName = "AscendiveTest"
 
-export const DatabaseAuth = { username: "cader", password:'@/t!MmEASd"~-]zwlfYK8qhhK'}
+export const DatabaseAuth = { username: "react", password: "react"}
