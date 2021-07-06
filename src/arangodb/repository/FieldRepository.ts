aeac435bcbc9f6111f07938a417999eb866dfb35
Cader Hancock <Cader.Hancock@gmail.com>
2021-07-06 14:59:18 -0700

    New query per Vincent.

--- src/arangodb/repository/FieldRepository.ts
+++ src/arangodb/repository/FieldRepository.ts
@@ -9,14 +9,18 @@ export class FieldRepository {
     async getFieldsByCollationUuid(uuid: String): Promise<any> {
         this.db.collection("Fields")
         const cursor = await this.db.query(aql`
-                Let collationKey = ${uuid}
-                FOR c IN Collations
-                FILTER(c.version.key == collationKey && c.version.isCurrent)
+            Let collationKey = ${uuid}
+            FOR c IN Collations
+            FILTER(c.version.key == collationKey && c.version.isCurrent)
+                FOR ct IN CollationTypes
+                    FILTER (ct.version.key == c.reference.collationType.key && ct.reference.playbook.key == c.reference.playbook.key && ct.version.isCurrent)
+                    FOR ctf in ct.field[*]
+                    FILTER (ctf.isActive)
                     FOR cf in c.field[*]
+                        FILTER (cf.key == ctf.key)
                         FOR f IN Fields
-                        FILTER (f.version.key == cf.key && f.control.isActive && f.version.isCurrent)
-                        RETURN {"field":cf.title, "value":cf.value, "default":f.default, "control":f.control}
-                `) as ArrayCursor<any>
+                            FILTER (f.version.key == ctf.key && f.control.isActive && f.version.isCurrent)
+                            RETURN {"field":ctf.title, "value":cf.value, "default":f.default, "control":f.control}                `) as ArrayCursor<any>
         return cursor.all();
     }
     async test(): Promise<any[]> {
